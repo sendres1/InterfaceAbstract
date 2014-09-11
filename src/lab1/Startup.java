7c880380a4407bd88c647048a2c68927617a1d97
sendres1 <sendres1@L15203039988.wctc.msft>
2014-09-11 14:08:24 -0500

    startup added

---
+++ src/lab1/Startup.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab1;
+
+/**
+ *
+ * @author sendres1
+ */
+public class Startup {
+    
+     public static void main(String[] args) {
+    
+  //  new Course = new IntroJavaCourse(courseName, courseNumber)
+    
+    Course course1 = new IntroJavaCourse("course1", "11111");
+    
+//    Dog dog1 = new Dog();
+//        
+//        // all animals can do this...
+//        dog1.setAge(5);
+//        dog1.setName("Fido");
+//        System.out.print("Dog 1 says: ");
+//        dog1.speak();
+}
+}
