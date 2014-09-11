66e966f21039d210990518e0aee09889b4ddc794
emitchell7 <emitchell7@L15204039989.wctc.msft>
2014-09-11 12:07:30 -0500

    common fields out of sub classes commented out.  also add extends.

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class AdvancedJavaCourse {
+public class AdvancedJavaCourse extends Course {
 //    String courseName;
 //    private String courseNumber;
 //    private double credits;
