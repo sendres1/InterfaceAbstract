614559ec390b0ea50ef0e9ebf9e48374bfe86fd1
emitchell7 <emitchell7@L15204039989.wctc.msft>
2014-09-11 12:40:03 -0500

    12:33

--- src/lab1/Course.java
+++ src/lab1/Course.java
@@ -6,12 +6,14 @@
 
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author emitchell7
  */
 public class Course {
-     private String courseName;
+    private String courseName;
     private String courseNumber;
     private double credits; 
     
@@ -40,19 +42,52 @@ private double getCredits() {
         return courseNumber;
     }
 
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public void setCourseNumber(String courseNumber) {
+//    public void setCourseName(String courseName) {
+//        this.courseName = courseName;
+//    }
+//
+//    public void setCourseNumber(String courseNumber) {
+//        this.courseNumber = courseNumber;
+//    }
+//
+//    public void setCredits(double credits) {
+//        this.credits = credits;
+//    }
+ 
+ 
+  public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
         this.courseNumber = courseNumber;
     }
 
+//    public double getCredits() {
+//        return credits;
+//    }
+
     public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
         this.credits = credits;
     }
- 
- 
- 
+
+//    public String getCourseName() {
+//        return courseName;
+//    }
+
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
 
 }
