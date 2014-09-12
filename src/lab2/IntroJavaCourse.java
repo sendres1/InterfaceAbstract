4ad10de30c4b762b362fc67bd649c60a5245af72
sendres1 <sendres1@L15204039989.wctc.msft>
2014-09-12 13:10:23 -0500

    intro

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -1,12 +1,14 @@
 package lab2;
 
+import javax.swing.JOptionPane;
+
 /**
  * Describe responsibilities here.
  *
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse {
+public class IntroJavaCourse implements Course {
     String courseName;
     private String courseNumber;
     private double credits;
@@ -21,7 +23,12 @@ public class IntroJavaCourse {
         return courseNumber;
     }
 
-    public void setCourseNumber(String courseNumber) {
+     public void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
         this.courseNumber = courseNumber;
     }
 
@@ -29,17 +36,52 @@ public class IntroJavaCourse {
         return credits;
     }
 
-    public void setCredits(double credits) {
+     public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
         this.credits = credits;
     }
+      public void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
 
+    
     public String getPrerequisites() {
         return prerequisites;
     }
 
     public void setPrerequisites(String prerequisites) {
-        this.prerequisites = prerequisites;
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+//        this.prerequisites = prerequisites;
+    }
+    
+     
+        
+public final String getCourseName() {
+        return courseName;
+
     }
 
+public final String  getCapitalizedCourseName() {
+        return this.getCourseName().toUpperCase();
+    }
+
+
+
+
+    
+   
     
 }
